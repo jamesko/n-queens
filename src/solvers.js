5a61819f56e69d809d1e9bfd4bc86ff29c44246b
James Kolonusz <jamesak@gmail.com>
2014-12-18 18:00:15 -0800

    Add code for countNRooksSolutions.

--- src/solvers.js
+++ src/solvers.js
@@ -40,9 +40,15 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
+  var factorial = function(n) {
+    if(n == 0) {
+      return 1
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  return factorial(n); //solutionCount;
 };
 
 
