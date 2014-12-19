a93972000897a6dd6a640ae3fc629a82eaa2534e
James Kolonusz <jamesak@gmail.com>
2014-12-18 18:03:12 -0800

    Add code for findNQueensSolution.

--- src/solvers.js
+++ src/solvers.js
@@ -57,8 +57,39 @@ window.countNRooksSolutions = function(n) {
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var numbOfQueens = n;
+  var board = new Board({n:n});
+  function walkArray (row){
+    var isSolvable = false;
+    for(var i = 0; i < numbOfQueens; i++){
+      board.togglePiece(row, i);
+      if(board.hasAnyQueensConflicts()){
+        if(i === numbOfQueens - 1){
+          board.togglePiece(row, i);
+          isSolvable = false;
+          return isSolvable;
+        }
+        board.togglePiece(row, i);
+        continue;
+      }
+      if(row === n-1){  // this check for the last row
+        isSolvable = true;
+        return isSolvable;
+      } else {
+        isSolvable = walkArray(row +1);
+        if(isSolvable){
+          return isSolvable;
+        }
+        board.togglePiece(row, i);
+      }
+    }
+    return isSolvable;
+  }
+  walkArray(0);
+  return board.rows();
+
+  //console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  //return solution;
 };
 
 
